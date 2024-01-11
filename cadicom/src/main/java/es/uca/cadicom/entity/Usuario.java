41ea34d239d6c154864baee10b89034e4fa4aa60
Mwazoski <mwazoski96@yahoo.com>
2024-01-11 15:45:05 +0100

    changes

--- cadicom/src/main/java/es/uca/cadicom/entity/Usuario.java
+++ cadicom/src/main/java/es/uca/cadicom/entity/Usuario.java
@@ -14,8 +14,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Entity
-public class Usuario  extends AbstractEntity implements UserDetails {
-
+public class Usuario extends AbstractEntity implements UserDetails{
     @Setter
     @Getter
     @Id
